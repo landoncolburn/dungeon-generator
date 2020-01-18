382c985243ab40f8e5953c0625f2c71ec7ae71ff
Landon Colburn <colburnlandon@gmail.com>
2020-01-18 17:30:26 -0600

    No GUI

--- DrunkenStumble.java
+++ DrunkenStumble.java
@@ -11,13 +11,13 @@ import java.awt.event.*;
 
 public class DrunkenStumble {
 
-  public static JFrame frame = new JFrame("Generator");
   public static Cell[][] cellList;
-  public static Dimension size = new Dimension(600,600);
+  public static Dimension size = new Dimension(60,60);
   public static ArrayList<Walker> walkerList = new ArrayList<Walker>();
 
   public static int cellGridHeight;
-  public static int cellGridWidth ;
+  public static int cellGridWidth;
+  public static int cellSize = 1;
 
   public static int counter = 0;
 
@@ -26,42 +26,26 @@ public class DrunkenStumble {
   }
 
   public DrunkenStumble(){
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(3);
-    frame.setPreferredSize(size);
-    frame.repaint();
-    frame.pack();
-
     initMap();
-    addCells();
     spawnWalkers();
     walkWalkers();
     generateWalls();
     removeSingles();
-    frame.repaint();
+    printMatrix();
   }
 
   public void initMap(){
     int count = 0;
-    cellGridHeight = (int)size.getHeight()/Cell.size;
-    cellGridWidth = (int)size.getWidth()/Cell.size;
+    cellGridHeight = (int)size.getHeight()/cellSize;
+    cellGridWidth = (int)size.getWidth()/cellSize;
     cellList = new Cell[cellGridHeight][cellGridWidth];
     for(int i = 0; i<cellGridHeight; i++){
       for(int j = 0; j<cellGridWidth; j++){
-        cellList[i][j] = new Cell(i*Cell.size, j*Cell.size, 6);
+        cellList[i][j] = Cell.EMPTY;
         count++;
       }
     }
     System.out.println(count + " cells created succesfully.");
-  }
-
-  public void addCells(){
-    for(int i = 0; i<cellList.length; i++){
-      for(int j = 0; j<cellList[i].length; j++){
-        frame.add(cellList[i][j]);
-      }
-    }
-    frame.repaint();
   }
 
   public void spawnWalkers(){
@@ -79,36 +63,30 @@ public class DrunkenStumble {
         Walker w = walkerList.get(j);
         w.walk();
         if(w.getX() >= 0 && w.getX() < 60 && w.getY() >= 0 && w.getY() < 60){
-          cellList[w.getX()][w.getY()].setCol(1);
+          cellList[w.getX()][w.getY()]=Cell.FLOOR;
           counter++;
         }
       }
       if(counter > 400){
         break;
       }
-      try{
-        Thread.sleep(100);
-      } catch(Exception e){
-
-      }
-
     }
   }
 
   public void removeSingles(){
     for(int i = 0; i<cellList.length; i++){
       for(int j = 0; j<cellList[i].length; j++){
-        if(cellList[i][j].getCol().equals(Color.BLACK)){
+        if(cellList[i][j].equals(Cell.WALL)){
           int count = 0;
           for(int k = -1; k <= 1; k++){
             for(int l = -1; l <= 1; l++){
-              if(cellList[i+k][j+l].getCol().equals(Color.WHITE)){
+              if(cellList[i+k][j+l].equals(Cell.FLOOR)){
                 count++;
               }
             }
           }
           if(count==8){
-            cellList[i][j].setCol(1);
+            cellList[i][j] = Cell.FLOOR;
           }
         }
       }
@@ -121,22 +99,39 @@ public class DrunkenStumble {
         if(i==0 || j==0 || i==cellList.length-1 || j==cellList[i].length-1){
 
         } else {
-          if(cellList[i][j].getCol().equals(Color.WHITE)){
+          if(cellList[i][j].equals(Cell.FLOOR)){
             for(int k = -1; k <= 1; k++){
               for(int l = -1; l <= 1; l++){
-                if(cellList[i+k][j+l].getCol().equals(Color.DARK_GRAY)){
-                  try{
-                    Thread.sleep(10);
-                  } catch(Exception e){
-
-                  }
-                  cellList[i+k][j+l].setCol(0);
+                if(cellList[i+k][j+l].equals(Cell.EMPTY)){
+                  cellList[i+k][j+l] = Cell.WALL;
                 }
               }
             }
           }
         }
       }
+    }
+  }
+
+  public void printMatrix(){
+    for(int i = 0; i<cellList.length; i++){
+      for(int j = 0; j<cellList[i].length; j++){
+        switch(cellList[i][j]){
+          case EMPTY:
+            System.out.print(" ");
+            break;
+          case WALL:
+            System.out.print("=");
+            break;
+          case FLOOR:
+            System.out.print(".");
+            break;
+          default:
+            System.out.print(" ");
+            break;
+        }
+      }
+      System.out.println("");
     }
   }
 
